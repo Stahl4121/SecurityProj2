2a2f7947f686e645fa1da229bc4e721eb6fa75b0
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2019-11-12 16:10:39 -0500

    ^^

--- pass_store.c
+++ pass_store.c
@@ -155,13 +155,13 @@ int pass_store_add_user(const char *username, const char *password)
   int repeat_username = 0;
   user_pass_t *passwords = NULL;
   size_t num_pass_out = 0;
-  pass_store_load(&passwords, &num_pass_out);
+  __pass_store_load(&passwords, &num_pass_out);
 
   /////////////////////////////////////
   /// CHECK FOR DUPLICATE USERNAMES ///
   /////////////////////////////////////
   for(int i = 0; i < num_pass_out; i++) {
-    if(username = passwords[i].username) repeat_username = 1;
+    if(username == passwords[i].username) { repeat_username = 1;}
   }
 
   if(!repeat_username) {
@@ -171,9 +171,9 @@ int pass_store_add_user(const char *username, const char *password)
     fprintf(stderr, "in pass_store_add_user \n");
     // create password's salt
     unsigned char salt[SALT_LEN];
-    fprintf(stderr, "after salt = null \n");
+    //fprintf(stderr, "after salt = null \n");
     if(!RAND_bytes(salt, SALT_LEN)) ret = -1; 
-    fprintf(stderr, "the salt is: %s", salt);
+    //fprintf(stderr, "the salt is: %s", salt);
 
     //////////////////////////////
     /// BASE64 ENCODE THE SALT ///
@@ -185,80 +185,57 @@ int pass_store_add_user(const char *username, const char *password)
     BIO *enc_salt_bio = BIO_new(BIO_s_mem());
     // chain the Base64 filter to the memory buffer sink
     BIO_push(b64_salt_bio, enc_salt_bio);
-    // // Base64 encoding by default contains new lines.
-    // // Do not output new lines.
-    // BIO_set_flags(b64_salt_bio, BIO_FLAGS_BASE64_NO_NL);
-    // // Input salt into the Base64 filter and flush the filter.
+    // Base64 encoding by default contains new lines.
+    // Do not output new lines.
+    BIO_set_flags(b64_salt_bio, BIO_FLAGS_BASE64_NO_NL);
+    // Input salt into the Base64 filter and flush the filter.
     BIO_write(b64_salt_bio, salt, SALT_LEN);
     BIO_flush(b64_salt_bio);
 
     // Get pointer and length of data in the memory buffer sink
-    char *b64_salt[SALT_LEN_BASE64];
+    unsigned char b64_salt[SALT_LEN_BASE64];
     if(SALT_LEN_BASE64 != BIO_get_mem_data(enc_salt_bio, &b64_salt)) ret = -1;
-    fprintf(stderr, "base64 salt: %s", b64_salt);
+    //fprintf(stderr, "base64 salt: %s", b64_salt);
     
     ///////////////////////////////////////////
     // CONCATENATE PASSWORD AND BASE64 SALT ///
     ///////////////////////////////////////////
     int pass_len = strlen(password);
     int pass_and_salt_len = pass_len + SALT_LEN_BASE64 + 1;
-    char pass_and_salt[pass_and_salt_len];
-    strncat(pass_and_salt, password, pass_len);
-    strncat(pass_and_salt, b64_salt, SALT_LEN_BASE64);
-    fprintf(stderr, "concatenated password and salt: %s", pass_and_salt);
+    unsigned char *pass_and_salt;
+    pass_and_salt = malloc(sizeof(pass_and_salt_len));
+    memcpy(pass_and_salt, password, pass_len);
+    memcpy(pass_and_salt, b64_salt, SALT_LEN_BASE64);
+    //fprintf(stderr, "concatenated password and salt: %s", pass_and_salt);
     
     ////////////////////////////////////////
     /// SHA 512 PASSWORD AND BASE64 SALT ///
     ////////////////////////////////////////
-    char *sha_pass_salt[SHA512_DIGEST_LENGTH];
-    SHA512(pass_and_salt, pass_and_salt_len, sha_pass_salt);
+    uint8_t sha_pass_salt[SHA512_DIGEST_LENGTH];
+    SHA512(pass_and_salt, pass_and_salt_len, (unsigned char*)sha_pass_salt);
     
-    /////////////////////////////////////////////////////////
-    /// B64 THE SHA512 USERNAME WITH SALTED PASSWORD HASH ///
-    /////////////////////////////////////////////////////////
+    user_pass_t new_pass_entry;
+    // clear out the memory of the new struct
+    memset(&new_pass_entry, 0, sizeof(new_pass_entry));
+    // use strncpy for username
+    // use bio to copy the salt
+    // use mem copy for password hash
+    strncpy(new_pass_entry.username, username, strlen(username));
+    memcpy(new_pass_entry.pass_hash, sha_pass_salt, SHA512_DIGEST_LENGTH);
+    memcpy(new_pass_entry.salt, b64_salt, SALT_LEN_BASE64+1);
     
-    // Base64 filter
-    BIO *b64_salt_and_pass_bio = BIO_new(BIO_f_base64());
-    // Memory buffer sink
-    BIO *enc_salt_and_pass_bio = BIO_new(BIO_s_mem());
-    // chain the Base64 filter to the memory buffer sink
-    BIO_push(b64_salt_and_pass_bio, enc_salt_and_pass_bio);
-    // Base64 encoding by default contains new lines.
-    // Do not output new lines.
-    BIO_set_flags(b64_salt_and_pass_bio, BIO_FLAGS_BASE64_NO_NL);
-    // Input data into the Base64 filter and flush the filter.
-    BIO_write(b64_salt_and_pass_bio, sha_pass_salt, SHA512_DIGEST_LENGTH);
-    BIO_flush(b64_salt_and_pass_bio);
-
-    // Get pointer and length of data in the memory buffer sink
-    char *b64_pass_and_salt[SHA512_DIGEST_LENGTH_BASE64];
-    BIO_get_mem_data(enc_salt_and_pass_bio, &b64_pass_and_salt);
-
-    
-    struct user_pass_t new_pass_entry;
-    new_pass_entry.username = username;
-    new_pass_entry.pass_hash = b64_pass_and_salt;
-    new_pass_entry.salt = salt;
-
     ///////////////////////////////////////////////////
     /// NEED TO ADD new_pass_entry TO **passwords /////
     /// AND NEED TO ADD THE STRING BELOW TO THE TXT ///
     ///////////////////////////////////////////////////
     
-    // the string format is below, the struct is above. I don't have 
-    // it set up correctly with her helper functions
-    // Dr. Al Moakar wanted us to be using the password 
-    // struct array and writing to the txt file
-
-    // username:$6$[encoded password salt]$[encoded salted password hash]
-    pass_store_save(passwords, num_pass_out + 1, 1)
+    __pass_store_save(&new_pass_entry, 1, 1);
     
     // Finally, free the BIO objects
     BIO_free_all(b64_salt_bio);
-    BIO_free_all(b64_salt_and_pass_bio);
   }
   
-  return 0;
+  return ret;
 }
 
 
@@ -294,6 +271,8 @@ int pass_store_remove_user(const char *username)
  */
 int pass_store_check_password(const char *username, const char *password)
 {
+  // check that username exists
+
   // copy paste from the pass_store_add_user to regenerate the 
   // encoded password
 
